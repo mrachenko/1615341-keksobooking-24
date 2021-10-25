ae3f8b659183a440145ba99db9098d8570834bc3
Alex <sashamarhenko@gmail.com>
2021-10-25 20:26:21 +0300

    исправил вывод для пустых массивов фото и фичи

--- js/cards.js
+++ js/cards.js
@@ -37,20 +37,27 @@ outputArray.forEach((outputArrayItem) => {
       break;
     default:
       cardElement.querySelector('.popup__text--address').classList.add('hidden');
+      break;
   }
   prepareOutputField((outputArrayItem.offer.rooms || outputArrayItem.offer.guests), cardElement.querySelector('.popup__text--capacity'), `${outputArrayItem.offer.rooms} комнаты для ${outputArrayItem.offer.guests} гостей` );
   prepareOutputField((outputArrayItem.offer.checkin || outputArrayItem.offer.checkout), cardElement.querySelector('.popup__text--time'), `Заезд после ${outputArrayItem.offer.checkin}, выезд до ${outputArrayItem.offer.checkout}`);
   prepareOutputField(outputArrayItem.offer.description, cardElement.querySelector('.popup__description'));
   prepareOutputField(outputArrayItem.author.avatar, cardElement.querySelector('.popup__avatar'));
 
-  featureList.forEach((featureListItem) => {
-    const isNecessary = outputArrayItem.offer.features.some(
-      (userEmotion) => featureListItem.classList.contains(`popup__feature--${userEmotion}`),
-    );
-    if (!isNecessary) {
-      featureListItem.remove();
-    }
-  });
+  if (outputArrayItem.offer.features.length > 0) {
+    featureList.forEach((featureListItem) => {
+      const isNecessary = outputArrayItem.offer.features.some(
+        (userEmotion) => featureListItem.classList.contains(`popup__feature--${userEmotion}`),
+      );
+      if (!isNecessary) {
+        featureListItem.remove();
+      }
+    });
+  }
+  else {
+    featureContainer.classList.add('hidden');
+  }
+
   if (outputArrayItem.offer.photos.length > 0) {
     for (let i = 1; i < outputArrayItem.offer.photos.length; i++) {
       const appendPhoto = photoList[0].cloneNode(true);
