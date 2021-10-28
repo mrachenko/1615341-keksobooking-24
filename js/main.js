d4bbf4a89d8c00c200ac3d91993f314bc42cafa4
Alex <sashamarhenko@gmail.com>
2021-10-28 22:34:50 +0300

    добавил функции

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 import { similarCardFragment } from './cards.js';
+import { makePageActive, makePageInactive } from './activity.js';
 
 const mapPlace = document.querySelector('.map__canvas');
 mapPlace.appendChild(similarCardFragment.firstElementChild);
+makePageInactive();
+makePageActive();
